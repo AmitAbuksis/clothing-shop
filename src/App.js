0bd3e3cf30e6d7af3b2a4309f2ce7f401f40ff20
Amit Abuksis <Amitabuksis8@gmail.com>
2020-04-27 15:17:21 +0300

    update menu item to nav withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import HomePage from './pages/homepage/HomePage'
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage =() => {
+  return (<div>
+      <h1>HATS PAGE</h1>
+  </div>)
+}
 
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+        <Route path='/' component={HomePage} exact/>
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
   );
 }
 export default App;
