8ac8c7c99e11e4e4d7a2dbc7f526c21d772e4dc6
Amit <amitabuksis8@gmail.com>
2020-05-12 10:20:59 +0300

    add redux and implement userAction to header and app components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/userActions'
 import HomePage from './pages/homepage/HomePage'
 import ShopPage from './pages/shop/ShopPage';
 import Header from './components/header/Header';
@@ -11,29 +13,21 @@ import './App.css';
 
 class App extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unSubscribeFromAuth = null ;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUSerProfileDocument(userAuth);
         userRef.onSnapshot(snapshot => {
-          this.setState({
-            currentUser: {
+        setCurrentUser({
               id: snapshot.id,
               ...snapshot.data()
-            }
+            });
           });
-        });
-      }
-      this.setState({currentUser: userAuth});
+        }
+        setCurrentUser(userAuth);
     });
   }
 
@@ -45,7 +39,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header/>
         <Switch>
           <Route path='/' component={HomePage} exact/>
           <Route path='/shop' component={ShopPage} />
@@ -56,4 +50,9 @@ class App extends React.Component {
   }
   
 }
-export default App;
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
